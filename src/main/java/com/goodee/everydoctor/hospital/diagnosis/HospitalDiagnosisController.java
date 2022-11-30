28331101014304bda1f651da42d49be0d23fea4e
kommo11 <djqpsdj@naver.com>
2022-12-01 07:38:37 +0900

    병원 진료

---
+++ src/main/java/com/goodee/everydoctor/hospital/diagnosis/HospitalDiagnosisController.java
@@ -0,0 +1,11 @@
+package com.goodee.everydoctor.hospital.diagnosis;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hospital/diagnosis/*")
+public class HospitalDiagnosisController {
+
+	
+}
