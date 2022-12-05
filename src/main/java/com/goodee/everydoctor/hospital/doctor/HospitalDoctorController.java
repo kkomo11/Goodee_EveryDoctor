64481a686a0ac74691d218cb618b8dcce55d785e
kkomo11 <djqpsdj@naver.com>
2022-12-05 17:02:28 +0900

    의사 리스트 DB

--- src/main/java/com/goodee/everydoctor/hospital/doctor/HospitalDoctorController.java
+++ src/main/java/com/goodee/everydoctor/hospital/doctor/HospitalDoctorController.java
@@ -1,12 +1,20 @@
 package com.goodee.everydoctor.hospital.doctor;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/hospital/doctor/*")
-public class DoctorController {
+public class HospitalDoctorController {
+	
+	@Autowired
+	private HospitalDoctorService hospitalDoctorService;
 	
 	@GetMapping("detail")
 	public String doctorDetail() throws Exception {
@@ -21,5 +29,14 @@ public class DoctorController {
 	@GetMapping("/diagnosis/detail")
 	public String diagnosisDetail() throws Exception {
 		return "hospital/diagnosisDetail";
+	}
+	
+	@GetMapping("/list")
+	@ResponseBody
+	public ModelAndView doctorList(ModelAndView modelAndView) throws Exception {
+		List<HospitalDoctorVO> doctorList = hospitalDoctorService.findDoctorList();
+		modelAndView.addObject("doctorList", doctorList);
+		modelAndView.setViewName("hospital/doctorList");
+		return modelAndView;
 	}
 }
