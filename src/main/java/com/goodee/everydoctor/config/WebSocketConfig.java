50849890bd190ac00b51243e0524904f52df604a
kommo11 <djqpsdj@naver.com>
2022-12-06 02:31:17 +0900

    채팅 테스트

--- src/main/java/com/goodee/everydoctor/config/WebSocketConfig.java
+++ src/main/java/com/goodee/everydoctor/config/WebSocketConfig.java
@@ -1,56 +1,48 @@
 package com.goodee.everydoctor.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSocket
 @RequiredArgsConstructor
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketConfigurer, WebSocketMessageBrokerConfigurer{
-
-	@Autowired
-	private final ChatHandler chatHandler;
+public class WebSocketConfig implements WebSocketConfigurer {
 	
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(chatHandler, "ws/chat")
-				.setAllowedOrigins("*");
-		
 		registry.addHandler(signalHandler(), "/signal")
-        		.setAllowedOrigins("*"); // allow all origins
+				.setAllowedOriginPatterns("http://*:81", "http://*.*.*.*:81")
+		        .withSockJS()
+		        .setClientLibraryUrl("https://cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.1.2/sockjs.js");
+		        //.setClientLibarayUrl은 그냥 sockjs CDN 주소를 입력해도 무관하다.
+		        //https://cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.1.2/sockjs.js
+		        // 원래 코드 : http://localhost:8080/myapp/js/sock-client.js
+		        /*
+		        Spring Boot에서 CORS 설정 시, .allowCredentials(true)와
+		        .allowedOrigins("*")는 동시 설정을 못하도록 업데이트 되었다고 한다.
+		        모든 주소를 허용하는 대신 특정 패턴만 허용하는 것으로 적용해야한다고 변동됨.
+		        
+		        .allowedOrigins("*") 대신 .allowedOriginPatterns("*")를 사용하면 에러는 해결이
+		        된다고 한다.
+		        
+		        나는 이처럼 하지 않고, http://localhost:8080 또는, IP 주소로 접속하기 때문에
+		        위에 설정처럼 하였다.
+		        */
 	}
-	
-	@Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS(); //웹 소캣을 사용하기 위해 설정하는 부분
-    }
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app"); //prefix 설정
-        registry.enableSimpleBroker("/topic"); //topic 이라는 주제에 브로커를 설정
-    }
-    
-	@Bean
-    public WebSocketHandler signalHandler() {
+    @Bean
+    WebSocketHandler signalHandler() {
         return new SignalHandler();
     }
 
-	@Bean
-    public ServletServerContainerFactoryBean createWebSocketContainer() {
+    @Bean
+    ServletServerContainerFactoryBean createWebSocketContainer() {
         ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
         container.setMaxTextMessageBufferSize(8192);
         container.setMaxBinaryMessageBufferSize(8192);
