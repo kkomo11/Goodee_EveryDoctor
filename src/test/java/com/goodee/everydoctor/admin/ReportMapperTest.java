1f7845706496c02c74b8c08cbd14cd03cb2157bc
Namjin-kimm <namjin-kim@daum.net>
2022-12-06 19:41:31 +0900

    테스트

---
+++ src/test/java/com/goodee/everydoctor/admin/ReportMapperTest.java
@@ -0,0 +1,16 @@
+package com.goodee.everydoctor.admin;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportMapperTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
