516a3403a65f2229aeef073e2338f98609b6978f
yez-try <skeosk424@gmail.com>
2022-12-06 19:14:34 +0900

    UserVO authorities null인 경우의 처리, UserMapper에 롤 넣는 매퍼 넣음

--- src/main/java/com/goodee/everydoctor/user/UserMapper.java
+++ src/main/java/com/goodee/everydoctor/user/UserMapper.java
@@ -8,6 +8,12 @@ public interface UserMapper {
 
 	public int inputUser(UserVO userVO) throws Exception;
 	public int inputRolePre(UserVO userVO)throws Exception;
+	public int inputRoleMember(UserVO userVO)throws Exception;
+	public int inputRoleDoctor(UserVO userVO)throws Exception;
+	public int inputRolePetdoc(UserVO userVO)throws Exception;
+	public int inputRoleNurse(UserVO userVO)throws Exception;
+	public int inputRolePetnurse(UserVO userVO)throws Exception;
+	public int inputRoleAdmin(UserVO userVO)throws Exception;
 	public UserVO getUserByUsername(String username)throws UsernameNotFoundException;
 	public int modifyUserMember(UserVO userVO) throws Exception;
 	public int modifyRoleMember(UserVO userVO) throws Exception;
