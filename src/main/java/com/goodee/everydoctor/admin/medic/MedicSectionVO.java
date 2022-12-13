f37aa9c2e2cd81303fd0e8ec1764be5ea2d3b356
Namjin-kimm <namjin-kim@daum.net>
2022-12-13 14:27:57 +0900

    제휴 신청 진료과목 선택 추가

---
+++ src/main/java/com/goodee/everydoctor/admin/medic/MedicSectionVO.java
@@ -0,0 +1,12 @@
+package com.goodee.everydoctor.admin.medic;
+
+import lombok.Data;
+
+@Data
+public class MedicSectionVO {
+	
+	private Integer medicSectionNum;
+	private String username;
+	private Integer sectionNum;
+
+}
