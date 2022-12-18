dfbee7c531337ce8bc3f9d0f46c6789e85cb5b15
simyeji <skeosk424@gmail.com>
2022-12-19 02:53:31 +0900

    내 진료내역

--- src/main/java/com/goodee/everydoctor/user/diagnosis/UserDiagnosisService.java
+++ src/main/java/com/goodee/everydoctor/user/diagnosis/UserDiagnosisService.java
@@ -15,12 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.goodee.everydoctor.file.FileVO;
+import com.goodee.everydoctor.hospital.diagnosis.HospitalDiagnosisVO;
 import com.goodee.everydoctor.user.address.UserAddressMapper;
 import com.goodee.everydoctor.user.address.UserAddressVO;
 import com.goodee.everydoctor.user.security.LogoutHandlerImpl;
 import com.goodee.everydoctor.util.EmailManager;
 import com.goodee.everydoctor.util.FileManager;
 import com.goodee.everydoctor.util.MessageManager;
+import com.goodee.everydoctor.util.Pager;
 import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.response.Certification;
 import com.siot.IamportRestClient.response.IamportResponse;
@@ -32,5 +34,17 @@ import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 @Slf4j
 @Transactional(rollbackFor = Exception.class)
 public class UserDiagnosisService {
+	
+	@Autowired
+	private UserDiagnosisMapper diagnosisMapper;
+	
+	public List<HospitalDiagnosisVO> getMylist(Pager pager)throws Exception{
+		Long cnt = diagnosisMapper.findMylistCnt(pager);
+		log.info("count {}", cnt);
+		pager.getNum(cnt);
+		pager.getRowNum();
+		
+		return diagnosisMapper.findHospitalDiagnosisListbyUsername(pager);
+	}
 	
 }
