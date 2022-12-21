2b4bb4d2b9ddf94d1f90b07674c3ba1b0a0b9328
simyeji <skeosk424@gmail.com>
2022-12-22 01:57:15 +0900

    회원가입 js 완료

--- src/main/resources/static/js/user/registration.js
+++ src/main/resources/static/js/user/registration.js
@@ -6,6 +6,7 @@ let NAMEchk = false;
 //let EMAILchk = false;
 let PASSWORDchk = false;
 let PASSWORDchk2 = false;
+let AGREchk = false;
 
 //아이디 체크
 $("#username").change(function(){
@@ -14,7 +15,6 @@ $("#username").change(function(){
     IDdupl = false;
     
     let idCheck = str.match(rex);
-    console.log();
     if(idCheck == null) {
 		IDchk = false;
 		$('#errorUsername').html('5~20자의 소문자, 숫자만 사용가능합니다. 첫글자는 소문자로 입력해주세요');
@@ -26,8 +26,6 @@ $("#username").change(function(){
 
 //아이디 중복체크
 $("#chkId").click(function(){
-    console.log("클릭", $("#username").val())
-    IDdupl = true;
 
     $.get("/user/chkId?username="+$("#username").val(), function(dt){
         if(dt>0){
@@ -36,7 +34,6 @@ $("#chkId").click(function(){
             alert("사용가능한 아이디입니다.");
             IDdupl = true;
         }
-        console.log(IDdupl)
     })
 })
 //이름 체크
@@ -45,7 +42,6 @@ $("#name").change(function() {
     let str = $("#name").val();
     
     NAMEchk = str.match(rex);
-    console.log(NAMEchk);
     if(NAMEchk == null) {
 		NAMEchk = false;
 		$('#errorName').html('2-5자의 한글을 입력해주세요');
@@ -62,7 +58,6 @@ $("#password").change(function() {
     PASSWORDchk2 = false;
     
     PASSWORDchk = str.match(rex);
-    console.log(PASSWORDchk);
     if(PASSWORDchk == null) {
 		PASSWORDchk = false;
 		$('#errorPassword').html('대문자, 소문자, 숫자로 구성된 6-20자의 비밀번호를 입력해주세요');
@@ -72,9 +67,11 @@ $("#password").change(function() {
 	}
 })
 //패스워드 2차 체크
-$("#password2").change(function() {
+$("#password2").blur(function() {
     let str = $("#password").val();
     let str2 = $("#password2").val();
+    
+    console.log("패스워드 체크"+str + "패스워드 체크"+str2)
     
     if(str != str2) {
 		PASSWORDchk2 = false;
@@ -87,7 +84,6 @@ $("#password2").change(function() {
 
 //전체동의 체크박스 클릭하면
 $("#agree_all").click(function(e){
-	console.log("헬로헬로",$(e.target).prop("checked"))
 	let agre_chk = $(e.target).prop("checked");
 	
 	if(agre_chk){
@@ -99,7 +95,6 @@ $("#agree_all").click(function(e){
 })
 // 동의사항 체크박스 클릭하면
 $(".little-check").click(function(e){
-	console.log($(e.target).prop("checked"))
 	let little_agre_chk = $(e.target).prop("checked");
 	
 	if(little_agre_chk){
@@ -120,11 +115,28 @@ $(".little-check").click(function(e){
 
 //회원가입 버튼 클릭
 $("#regiBtn").click(function(){
-	console.log("hello",IDchk,IDdupl,NAMEchk,PASSWORDchk,PASSWORDchk2	)
+	console.log("hello",$("#agree_all").prop("checked")	)
+	AGREchk = $("#agree_all").prop("checked");
 	
-	if(IDchk&IDdupl&NAMEchk&PASSWORDchk&PASSWORDchk2){
-		$("#regiForm").submit();
+	if(AGREchk){
+		
+		if(IDchk&IDdupl&NAMEchk&PASSWORDchk&PASSWORDchk2){
+			$("#regiForm").submit();
+		}else{
+			let message = (IDchk?"":"아이디 ");
+			message = message+ (IDdupl?"":"아이디 중복 ");
+			message = message+ (NAMEchk?"":"이름 ");
+			message = message+ (PASSWORDchk?"":"비밀번호 ");
+			message = message+ (PASSWORDchk2?"":"비밀번호 일치");
+			message = message+ "를 확인해주세요" 
+			
+			
+			alert(message);
+		}
+	}else{
+		alert("약관을 전체동의 해주세요")
 	}
+	
 
 	
 })
